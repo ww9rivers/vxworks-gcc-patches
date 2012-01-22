9e50fb2a069e8b863b4d73e387b333c640ab0b88
Cody Schafer <jmesmon@gmail.com>
2012-01-22 03:47:11 -0500

    Builds gcc reliably, libstdc++ build broken.
    powerpc-wrs-vxworks-g++ -x c++-header -nostdinc++ -DNOMINMAX
    -I/home/nyx/o/vxworks-gcc-patches/build5/libstdc++/include/powerpc-wrs-vxworks
    -I/home/nyx/o/vxworks-gcc-patches/build5/libstdc++/include
    -I/home/nyx/o/vxworks-gcc-patches/src5/gcc-4.6.2/libstdc++-v3/libsupc++ -O2 -g
    /home/nyx/o/vxworks-gcc-patches/src5/gcc-4.6.2/libstdc++-v3/include/precompiled/stdc++.h
    -o powerpc-wrs-vxworks/bits/stdc++.h.gch/O2g.gch
    In file included from
    /home/nyx/o/vxworks-gcc-patches/build5/libstdc++/include/atomic:41:0,
                     from
    		 /home/nyx/o/vxworks-gcc-patches/src5/gcc-4.6.2/libstdc++-v3/include/precompiled/stdc++.h:99:
    		 /home/nyx/o/vxworks-gcc-patches/build5/libstdc++/include/bits/atomic_base.h:214:25:
    		 error: 'int_least8_t' was not declared in this scope

--- build.bash
+++ build.bash
@@ -1,123 +1,270 @@
 #!/bin/bash
 
 # BEGIN CONFIGURATION
-BINUTILS_VERSION=2.21
-GCC_VERSION=4.5.2
-MPFR_VERSION=3.0.1
+BINUTILS_VERSION=2.22
+GCC_VERSION=4.6.2
+MPFR_VERSION=3.1.0
 GMP_VERSION=5.0.2
-MPC_VERSION=0.8.2
+MPC_VERSION=0.9
 JOBS=4
 # END CONFIGURATION
 
-function die()
-{
+die() {
     echo "$1"
     exit 1
 }
 
-[ -e build.bash ] || die "Must cd into source directory"
-PATCHROOT="$(pwd)"
+SEC_NUM=0
 
-[ $# -eq 3 ] || die "usage: build.bash PREFIX SRC BUILD"
-PREFIX="$1"
-SRC="$2"
-BUILD="$3"
-
-function download()
-{
-    [ -e "$SRC/$1" ] || wget -O "$SRC/$1" "$2"
+set_title () {
+	case $TERM in
+		xterm*|rxvt*|Eterm|aterm|kterm|gnome*|interix)
+			echo -ne "\033]0;$1\007"
+			;;
+		screen)
+			echo -ne "\033_$1\033\\"
+			;;
+		*)
+			einfo "Unsupported terminal for title setting"
+			;;
+	esac
 }
 
-export PATH="$PREFIX/bin:$PATH"
-export WIND_BASE="$PREFIX/powerpc-wrs-vxworks/wind_base"
+ebegin() {
+	einfo "$1 ..."
+	set_title "$1"
+	((SEC_NUM=SEC_NUM+1))
+}
 
-# Download
-echo "Downloading..."
-[ -d "$SRC" ] || mkdir "$SRC"
-download "gccdist.zip" "ftp://ftp.ni.com/pub/devzone/tut/updated_vxworks63gccdist.zip"
-download "binutils-$BINUTILS_VERSION.tar.bz2" "http://ftp.gnu.org/gnu/binutils/binutils-$BINUTILS_VERSION.tar.bz2"
-download "gcc-$GCC_VERSION.tar.bz2" "http://ftp.gnu.org/gnu/gcc/gcc-$GCC_VERSION/gcc-$GCC_VERSION.tar.bz2"
-download "mpfr-$MPFR_VERSION.tar.bz2" "http://www.mpfr.org/mpfr-current/mpfr-$MPFR_VERSION.tar.bz2"
-download "gmp-$GMP_VERSION.tar.bz2" "ftp://ftp.gmplib.org/pub/gmp-$GMP_VERSION/gmp-$GMP_VERSION.tar.bz2"
-download "mpc-$MPC_VERSION.tar.gz" "http://www.multiprecision.org/mpc/download/mpc-$MPC_VERSION.tar.gz"
+eend() {
+	((SEC_NUM=SEC_NUM-1))
+	einfo "$1 [ok]"
+}
 
-# Extract
-echo "Extracting files (this will take a long time)..."
-cd "$SRC"
-echo "  gccdist.zip"
-unzip -q gccdist.zip
-echo "  binutils-$BINUTILS_VERSION.tar.bz2"
-tar --bzip2 -xf "binutils-$BINUTILS_VERSION.tar.bz2"
-echo "  gcc-$GCC_VERSION.tar.bz2"
-tar --bzip2 -xf "gcc-$GCC_VERSION.tar.bz2"
-echo "  mpfr-$MPFR_VERSION.tar.bz2"
-tar --bzip2 -xf "mpfr-$MPFR_VERSION.tar.bz2"
-echo "  gmp-$GMP_VERSION.tar.bz2"
-tar --bzip2 -xf "gmp-$GMP_VERSION.tar.bz2"
-echo "  mpc-$MPC_VERSION.tar.gz"
-tar --gzip -xf "mpc-$MPC_VERSION.tar.gz"
+einfo() {
+	echo -ne "\033[1;30m>\033[0;36m>\033[1;36m> \033[0m${@}\n"
+}
 
-# Patch!
-echo "Patching..."
-cd "$SRC/gccdist/WindRiver/vxworks-6.3/target/h"
-sed "s:PREFIX:$(echo $PREFIX):" < "$PATCHROOT/vxworks-headers.patch" | patch -p1
-cd "$SRC/gcc-$GCC_VERSION"
-patch -p2 < "$PATCHROOT/gcc-diff.patch" # -p2 intentional.
+ewarn() {
+	echo -ne "\033[1;30m>\033[0;33m>\033[1;33m> \033[0m${@}\n"
+}
 
-# Set up headers
-echo "Headers..."
-[ -d "$PREFIX" ] || mkdir "$PREFIX"
-mkdir -p "$WIND_BASE/target"
-ln -s "$PREFIX/powerpc-wrs-vxworks/sys-include" "$WIND_BASE/target/h"
-cp -R "$SRC/gccdist/WindRiver/vxworks-6.3/host" "$WIND_BASE/host" # must be a copy, because build adds more.
+eerror() {
+	echo -ne "\033[1;30m>\033[0;31m>\033[1;31m> ${@}\033[0m\n"
+}
 
-# Build
-[ -d "$BUILD" ] || mkdir "$BUILD"
-cd "$BUILD"
-mkdir binutils gcc libstdc++
+skip_has () {
+	name="$1"
 
-echo "binutils:"
-cd "$BUILD/binutils"
-"$SRC/binutils-$BINUTILS_VERSION/configure" --prefix="$PREFIX" --target=powerpc-wrs-vxworks
-make -j "$JOBS" || die "** binutils build failed"
-make -j "$JOBS" install || die "** binutils install failed"
+	[ -z "$SKIP" ] && return 1
 
-echo "gcc:"
-ln -s "$SRC/mpfr-$MPFR_VERSION" "$SRC/gcc-$GCC_VERSION/mpfr"
-ln -s "$SRC/gmp-$GMP_VERSION" "$SRC/gcc-$GCC_VERSION/gmp"
-ln -s "$SRC/mpc-$MPC_VERSION" "$SRC/gcc-$GCC_VERSION/mpc"
-cd "$BUILD/gcc"
-"$SRC/gcc-$GCC_VERSION/configure" \
-    --prefix="$PREFIX" \
-    --target=powerpc-wrs-vxworks \
-    --with-gnu-as \
-    --with-gnu-ld \
-    --with-headers="$SRC/gccdist/WindRiver/vxworks-6.3/target/h" \
-    --disable-shared \
-    --disable-libssp \
-    --disable-multilib \
-    --with-float=hard \
-    --enable-languages=c,c++ \
-    --enable-threads=vxworks \
-    --without-gconv \
-    --disable-libgomp \
-    --disable-nls \
-    --disable-libmudflap \
-    --with-cpu-PPC603 \
-    CFLAGS="-g -O2" \
+	oIFS="$IFS"
+	IFS=":"
+	for s in $SKIP; do
+		[ "$s" = "$name" ] && {
+			IFS="$oIFS"
+			return 0
+		}
+	done
+	IFS="$oIFS"
+	return 1
+}
 
-make -j "$JOBS" || die "** gcc build failed"
-make -j "$JOBS" install || die "** gcc install failed"
+run () {
+	if skip_has "$1"; then
+		einfo "Skipping \"$1\""
+		return 0;
+	fi
 
-echo "libstdc++:"
-cd "$BUILD/libstdc++"
-"$SRC/gcc-$GCC_VERSION/libstdc++-v3/configure" \
-    --host=powerpc-wrs-vxworks \
-    --prefix="$PREFIX" \
-    --enable-libstdcxx-debug \
-    CFLAGS="-g -isystem $PREFIX/powerpc-wrs-vxworks/sys-include/ -isystem $PREFIX/powerpc-wrs-vxworks/sys-include/wrn/coreip/ -D_WRS_KERNEL -DCPU=PPC603 -DNOMINMAX" \
-    CXXFLAGS="-isystem $PREFIX/powerpc-wrs-vxworks/sys-include/ -isystem $PREFIX/powerpc-wrs-vxworks/sys-include/wrn/coreip/ -D_WRS_KERNEL -DCPU=PPC603 -DNOMINMAX" \
-    CPPFLAGS="-g -isystem $PREFIX/powerpc-wrs-vxworks/sys-include/ -isystem $PREFIX/powerpc-wrs-vxworks/sys-include/wrn/coreip/ -D_WRS_KERNEL -DCPU=PPC603 -DNOMINMAX"
+	ebegin "building \"$1\""
+	do_$1
+	eend   "$1"
+}
 
-make -j "$JOBS" || die "** libstdc++ build failed"
-make -j "$JOBS" install || die "** libstdc++ install failed" 
+extract () {
+	PD=$1
+	echo -n "ex $PD "
+	(
+	cd "$SRC"
+	case "$PD" in
+	*.tar.bz2)
+		tar -jxf "$PD"
+		;;
+	*.tar.gz)
+		tar -zxf "$PD"
+		;;
+	*.zip)
+		unzip -qo "$PD"
+		;;
+	*)
+		die "Don't know how to extract $PD"
+		;;
+	esac
+	[ $? -eq 0 ] || die "Could not extract \"$1\""
+	) || exit $?
+	echo "[ok]"
+}
+
+make_or_die() {
+	make -C "$BUILD/$1" -j "$JOBS" || die "** $1 build failed"
+	make -C "$BUILD/$1" -j "$JOBS" install || die "** $1 install failed"
+}
+
+download()
+{
+	dln="$(basename "$2")"
+	(
+	echo -n "dl $2 -> $1 "
+	cd "$SRC"
+	{
+		[ -e "$dln" ] || wget -c "$2" || exit $?
+		if [ "$dln" != "$1" ]; then
+			[ -e "$dln" ] && rm -f "$1" && \
+			ln "$dln" "$1" || exit 3
+		fi
+	} || die "[fail]"
+	) || exit && echo "[ok]"
+}
+
+[ -e build.bash ] || cd $(dirname $0)
+[ -e build.bash ] || die "Current working directory must be source directory"
+
+[ $# -eq 3 ] || [ $# -eq 4 ] || die "usage: build.bash PREFIX SRC BUILD [SKIPS]"
+PREFIX="$(realpath -m "$1")" # need an absolute here.
+SRC="$2"
+BUILD="$3"
+SKIP="${4:-}"
+
+# Must be absolute path as it is used in "conf", where we change directory.
+export WIND_BASE=$(realpath -m "$SRC/gccdist/WindRiver/vxworks-6.3")
+
+export PATH="$(realpath -m "$PREFIX/bin"):$PATH"
+
+[ -d "$SRC"    ] || mkdir "$SRC"    || exit
+[ -d "$PREFIX" ] || mkdir "$PREFIX" || exit
+[ -d "$BUILD"  ] || mkdir "$BUILD"  || exit
+for d in binutils gcc libstdc++; do
+	if ! skip_has "$d"; then
+		df="$BUILD/$d"
+		[ -d "$df" ] && rm -rf "$df"
+		mkdir -p "$df"
+
+		s="$SRC/$d"
+		if [ -d "$s" ]; then
+			rm -rf "$s"
+		fi
+	fi
+done
+
+conf ()
+{
+	PN="$1"
+	SAFE_PN="${1//+/x}"
+	(
+	echo "$BUILD/$PN"
+	cd "$BUILD/$PN" && eval "conf_$SAFE_PN"
+	) || die "conf of $PN failed"
+}
+
+conf_binutils ()
+{
+	"../../$SRC/binutils-$BINUTILS_VERSION/configure" \
+		--prefix="$PREFIX" \
+		--target=powerpc-wrs-vxworks \
+		--disable-nls
+}
+
+do_binutils () {
+	download "binutils-$BINUTILS_VERSION.tar.bz2" \
+		"http://ftp.gnu.org/gnu/binutils/binutils-$BINUTILS_VERSION.tar.bz2"
+	extract "binutils-$BINUTILS_VERSION.tar.bz2"
+	conf binutils
+	make_or_die binutils
+}
+
+run binutils
+
+#install_wrs_headers ()
+#{
+#	sed "s:PREFIX:$PREFIX:" < "vxworks-headers.patch" | patch -d "$SRC/gccdist/WindRiver/vxworks-6.3/target/h" -p1
+#
+#	mkdir -p "$WIND_BASE/target" || exit
+#	ln -fs "$PREFIX/powerpc-wrs-vxworks/sys-include" "$WIND_BASE/target/h" || exit
+#	cp -R  "$SRC/gccdist/WindRiver/vxworks-6.3/host" "$WIND_BASE/host" || exit # must be a copy, because build adds more.
+#}
+
+do_wrs_headers () {
+	download "gccdist.zip" \
+		"ftp://ftp.ni.com/pub/devzone/tut/updated_vxworks63gccdist.zip"
+	extract gccdist.zip
+#	install_wrs_headers
+}
+run wrs_headers
+
+prep_gcc ()
+{
+	patch -d "$SRC/gcc-$GCC_VERSION" -p1 < "gcc.patch" || exit 1
+	( cd "$SRC/gcc-$GCC_VERSION" && ./contrib/download_prerequisites ) || exit
+}
+
+conf_gcc ()
+{
+	"../../$SRC/gcc-$GCC_VERSION/configure" \
+	    --prefix="$PREFIX" \
+	    --target=powerpc-wrs-vxworks \
+	    --with-gnu-as \
+	    --with-gnu-ld \
+	    --with-headers="$WIND_BASE/target/h" \
+	    --disable-shared \
+	    --disable-libssp \
+	    --disable-multilib \
+	    --with-float=hard \
+	    --enable-languages=c,c++ \
+	    --enable-threads=vxworks \
+	    --without-gconv \
+	    --disable-libgomp \
+	    --disable-nls \
+	    --disable-libmudflap \
+	    --with-cpu-PPC603 \
+	    CFLAGS="-g -O2"
+}
+
+do_gcc () {
+	download "gcc-$GCC_VERSION.tar.bz2" \
+		"http://ftp.gnu.org/gnu/gcc/gcc-$GCC_VERSION/gcc-$GCC_VERSION.tar.bz2"
+	#download "mpfr-$MPFR_VERSION.tar.bz2" \
+	#	"http://www.mpfr.org/mpfr-current/mpfr-$MPFR_VERSION.tar.bz2"
+	#download "gmp-$GMP_VERSION.tar.bz2" \
+	#	"ftp://ftp.gmplib.org/pub/gmp-$GMP_VERSION/gmp-$GMP_VERSION.tar.bz2"
+	#download "mpc-$MPC_VERSION.tar.gz" \
+	#	"http://www.multiprecision.org/mpc/download/mpc-$MPC_VERSION.tar.gz"
+	#extract "mpfr-$MPFR_VERSION.tar.bz2"
+	#extract "gmp-$GMP_VERSION.tar.bz2"
+	#extract "mpc-$MPC_VERSION.tar.gz"
+
+	extract "gcc-$GCC_VERSION.tar.bz2"
+	prep_gcc
+	conf gcc
+	make_or_die gcc
+}
+run gcc
+
+conf_libstdcxx ()
+{
+	CPPFLAGS="-DNOMINMAX" CFLAGS="-DNOMINMAX" CXXFLAGS="-DNOMINMAX" \
+		"../../$SRC/gcc-$GCC_VERSION/libstdc++-v3/configure" \
+		--host=powerpc-wrs-vxworks \
+		--prefix="$PREFIX" \
+		--enable-libstdcxx-debug \
+
+	#	CFLAGS="-g -isystem $PREFIX/powerpc-wrs-vxworks/sys-include/ -isystem $PREFIX/powerpc-wrs-vxworks/sys-include/wrn/coreip/ -D_WRS_KERNEL -DCPU=PPC603 -DNOMINMAX" \
+	#	CXXFLAGS="-isystem $PREFIX/powerpc-wrs-vxworks/sys-include/ -isystem $PREFIX/powerpc-wrs-vxworks/sys-include/wrn/coreip/ -D_WRS_KERNEL -DCPU=PPC603 -DNOMINMAX" \
+	#	CPPFLAGS="-g -isystem $PREFIX/powerpc-wrs-vxworks/sys-include/ -isystem $PREFIX/powerpc-wrs-vxworks/sys-include/wrn/coreip/ -D_WRS_KERNEL -DCPU=PPC603 -DNOMINMAX"
+}
+
+do_libstdcxx () {
+	conf libstdc++
+	make_or_die libstdc++
+}
+
+run libstdcxx
